b0a0c20eeb3d515261073ac74fc1f0520feae92b
KJ <ezpost@gmail.com>
2017-11-19 22:50:52 +0800

    add navi bar

---
+++ compiler/code.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\kjtan\Desktop\KJ personal file\business plan\Customize post card\Website\Source code\ez-post.github.io\compiler"
+
+gulp serve
---
+++ ext/js/newfile.js
@@ -0,0 +1,4 @@
+//Begin MailChimp Signup Form
+style(type='text/css').
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+//End mc_embed_signup
